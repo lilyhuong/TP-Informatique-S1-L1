5bf66183ac64e1ca04a521e77dd95eec4d6523d4
lilyhuong <44270876+lilyhuong@users.noreply.github.com>
2018-11-12 09:58:08 +0100

    Add files via upload

---
+++ TP3.py
@@ -0,0 +1,16 @@
+mytuple = eval(input('entrer un tuple de  nombres '))
+mysum = 0
+n=0
+for nb in mytuple:
+    mysum = mysum + nb
+    n +=1
+print("the average of my tuple is", mysum/n)
+    
+##  
+##print(a)
+###
+##a = int(input('entre votre nombre'))
+##u0 = 0
+##u1 = 3 * u0 + 2
+##for i in range (0, ):
+##    
